c7e43db9a25398eeaed3c4abc26c981b4b38dbe0
Joao Almeida <joao-almeida11@outlook.com>
2025-07-22 19:46:59 +0100

    feat: qrcode styles

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -7,10 +7,10 @@ export default function QRCode() {
         <img alt="QRcode" src={QRCodeImage} />
       </figure>
       <div className="QRcode--details">
-        <h1 className="QRcode--details--title">
+        <h1 className="text-preset-1 QRcode--details--title">
           Improve your front-end skills by building projects
         </h1>
-        <p className="QRcode--details--description">
+        <p className="text-preset-2 QRcode--details--description">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
